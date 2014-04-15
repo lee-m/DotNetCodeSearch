afa7b00a9b5bfb91475ebe7f5a129286bc3378e7
Lee Millward <none@none>
2014-04-15 21:35:09 +0100

    Beginnings of implementing the ability to issue a search and show the results.

--- Client/public/javascripts/app.js
+++ Client/public/javascripts/app.js
@@ -1,27 +1,55 @@
 /*global angular*/
 
 //Create the main module and Elasticsearch client
-var codeSearchApp = angular.module('codeSearch', []);
+var codeSearchApp = angular.module('codeSearch', ['ngSanitize']);
 
 //Main controller
-codeSearchApp.controller('CodeSearchController', function ($scope, $http) {
+codeSearchApp.controller('CodeSearchController', function ($scope, $http, $filter) {
 
   'use strict';
 
-  $scope.data = 'default';
+  //Set to true if attempting to issue a query results in an error
+  $scope.searchError = false;
+
+  //'Template' search object for changesets which will highlight the hits in the message
+  $scope.searchTemplate = {
+    query: {
+      simple_query_string: {
+        query: "+critical +error",
+        default_operator: "AND"
+      },
+    },
+    highlight: {
+      pre_tags: ["<strong>"],
+      post_tags: ["</strong>"],
+      fields: {
+        message: {}
+      },
+    },
+    size: 50,
+  };
 
   $scope.searchButtonClicked = function() {
 
+    $http.post('http://localhost:9200/_search', $filter('json')($scope.searchTemplate))
+      .success(function (data, status, headers, config) {
+        $scope.searchResults = data.hits;
+      })
+      .error(function (data, status, headers, config) {
+        $scope.searchError = true;
+      });
+
+    /*
     $http({
         method: "GET",
         url: 'http://localhost:9200/_cluster/health?pretty=true',
       })
       .success(function (data, status, headers, config) {
-        $scope.data = data;
+        $scope.searchError = false;
       })
       .error(function (data, status, headers, config) {
-        $scope.data = 'Error';
-      });
+        $scope.searchError = true;
+      });*/
   };
 
 });
